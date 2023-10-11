b0697a74efd2e544c935f10b38f327f060992aee
beyzanuerdagi <beyzanur.erdagi@stu.fsm.edu.tr>
2023-10-11 18:05:48 +0300

    test

---
+++ Context/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Bitirme.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bitirme.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> contextOptions) 
+            : base(contextOptions)
+        {
+
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+
+    }
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,15 @@
+﻿namespace Bitirme.Models
+{
+    public class Employee
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public int Phone { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+
+
+    }
+}
