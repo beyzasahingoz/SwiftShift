75dedbc2d989a7de33b900dca78aed526de569ab
beyza <beyzasahingoz24@gmail.com>
2023-11-13 00:38:30 +0300

    added advert page

---
+++ Models/Adverts.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Bitirme.Models
+{
+    public class Adverts
+    {
+
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
+        public string Address { get; set; }
+        public string ProductKg { get; set; }
+        public string Note { get; set; }
+
+    }
+}
